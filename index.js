991e9b4be7ffd1eea9c68dd8361495ca0b575eba
LemonHaque <azizulhaquelemon31@gmail.com>
2023-05-30 10:28:34 +0600

    added delete method

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ulul2vm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,7 @@ async function run() {
 
         const menuCollection = client.db("bistroDb").collection("menu");
         const reviewCollection = client.db("bistroDb").collection("reviews");
+        const cartCollection = client.db("bistroDb").collection("carts");
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -37,6 +38,32 @@ async function run() {
         })
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        // cartCollection 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -62,3 +89,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`bistro boss is running on port${port}`);
 })
+
+
+
+/**
+ * -----Naming Convention---------
+ * 
+ * user :userCollection
+ * app.get('/users') ---> to get all user
+ * app.get('/users/:id') ---> to get a particular user
+ * app.post('/user') --> to create a new user
+ * app.patch('/user/:id')
+ * app.put('/users/:id')
+ * app.delete('/users/:id')
+ * 
+ */
